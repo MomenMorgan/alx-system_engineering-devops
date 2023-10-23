5d6db207c41ea6ffb52221ba21475faa5bf4afad
momin morgan <mominmorgan10@gmail.com>
2023-10-23 13:28:28 -0700

    hot fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 """Gather data from an API"""
-
+import json
 from requests import get
 from sys import argv
-import json
+
 
 if __name__ == "__main__":
     user_id = argv[1]
