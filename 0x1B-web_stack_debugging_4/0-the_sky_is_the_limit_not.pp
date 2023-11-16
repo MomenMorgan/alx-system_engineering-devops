d5eb757b599e208e15050601b81df9de30880a77
MominMorgan <mominmorgan10@gmail.com>
2023-11-16 21:31:30 +0200

    update ulimit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,16 @@
+# Increase the number of traffic for Nginx
+
+# Update the Ulimit 
+exec{'fix--for-nginx':
+   command => '/bin/sed -i "s/15/4096/g" /etc/default/nginx',
+
+   path => '/usr/bin:/usr/sbin:/bin',
+}
+
+# Restart the Nginx service
+
+exec{'restart-nginx':
+   command => '/etc/init.d/nginx restart',
+
+   path => '/etc/init.d/',
+}
---
+++ 0x1B-web_stack_debugging_4/REEADME.md
@@ -0,0 +1,1 @@
+Update the hard limit for Nginx to accept the apc requests
